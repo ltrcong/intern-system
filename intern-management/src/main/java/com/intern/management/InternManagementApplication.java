71b972b3c10920e7918c3ee9c3697718dd5c3076
letrongcong <letrongcong11062003@gmail.com>
2026-03-24 21:04:28 +0700

    init project

---
+++ intern-management/src/main/java/com/intern/management/InternManagementApplication.java
@@ -0,0 +1,13 @@
+package com.intern.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternManagementApplication.class, args);
+	}
+
+}
---
+++ intern-management/src/test/java/com/intern/management/InternManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
